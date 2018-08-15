8235d82ae7d25279552fbfe2a9035545b689f56d
florinivan <41228455+florinivan@users.noreply.github.com>
2018-08-15 08:41:58 +0200

    sql insert

---
+++ com.phimes.sic.business/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(1, 'mkyong','111@yahoo.com', TO_DATE('2018-08-11', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(2, 'yflow','222@yahoo.com', TO_DATE('2018-08-12', 'yyyy-mm-dd'));
+INSERT INTO "CUSTOMER" (ID, NAME, EMAIL, CREATED_DATE) VALUES(3, 'zilap','333@yahoo.com', TO_DATE('2018-08-13', 'yyyy-mm-dd'));
