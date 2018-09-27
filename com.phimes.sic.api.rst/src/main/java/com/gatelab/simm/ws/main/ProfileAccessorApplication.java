9c722ac6903adf955bf0d449c6b1fc8d28dcf9eb
florinivan <41228455+florinivan@users.noreply.github.com>
2018-09-27 08:59:44 +0200

    modify new project

---
+++ com.phimes.sic.api.rst/src/main/java/com/gatelab/simm/ws/main/ProfileAccessorApplication.java
@@ -0,0 +1,12 @@
+package com.gatelab.simm.ws.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileAccessorApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileAccessorApplication.class, args);
+	}
+}
--- com.phimes.sic.api.rst/src/test/java/com/phimes/sic/api/rst/controller/ProfileAccessorApplicationTests.java
+++ com.phimes.sic.api.rst/src/test/java/com/phimes/sic/api/rst/controller/ProfileAccessorApplicationTests.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.BDDAssertions.then;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {"management.port=0"})
-public class ProfileAccessorAppTests {
+public class ProfileAccessorApplicationTests {
 	
 	@LocalServerPort
 	private int port;
