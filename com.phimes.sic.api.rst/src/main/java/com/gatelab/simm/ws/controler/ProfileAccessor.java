c562ed6a5651885b4bb2ae01b6fb1742501b4c2d
madad <madad@DESKTOP-4DG82F1.station>
2018-09-28 19:19:08 +0200

    Aggiunta controller ProfileAccessor, manca ultimo metodo

--- com.phimes.sic.api.rst/src/main/java/com/gatelab/simm/ws/controler/ProfileAccessor.java
+++ com.phimes.sic.api.rst/src/main/java/com/gatelab/simm/ws/controler/ProfileAccessor.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.phimes.sic.api.dto.FilterDto;
+import com.phimes.sic.api.dto.LevelDto;
+import com.phimes.sic.api.dto.RoleDto;
 import com.phimes.sic.api.service.IProfileAccessorService;
-
 
 @Controller
 @RequestMapping(value = "/auth/profile/accessor")
@@ -24,5 +26,34 @@ public class ProfileAccessor {
 	public List<String> domainCodeListBy(@RequestParam(name="username", required=true) String usernameUsr) {
 		return profileAccessorService.getDomainCodeList(usernameUsr);
 	}
+	
+	@GetMapping("/login")
+	@ResponseBody
+	public void logIn(@RequestParam(name="usernameUsr", required=true)String usernameUsr,
+					  @RequestParam(name="password", required=true) String password,
+					  @RequestParam(name="domainCodeUsr, required=true") String domainCodeUsr) {
+		profileAccessorService.logIn(usernameUsr, password, domainCodeUsr);
+	}
+	
+	@GetMapping("/roles")
+	@ResponseBody
+	public List<RoleDto> rolesListBy(){
+		return profileAccessorService.getRoleList();
+	}
+	
+	@GetMapping("/levels")
+	@ResponseBody
+	public List<LevelDto> levelListBy(){
+		return profileAccessorService.getLevelList();
+	}
+	
+	@GetMapping("/filters")
+	@ResponseBody
+	public List<FilterDto> filterListBy(){
+		return profileAccessorService.getFilterList();
+	}
+	
+	//Manca ultimo metodo perchè non so come gestire i nomi in @getMapping (Mada)
+	
 
 }
